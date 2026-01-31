ed45e895c6a829dc90a9cf186b6e1821446a771e
HukovaNastya <hukovanasta@gmail.com>
2026-01-31 20:02:57 +0200

    added-Styles-to-the-text-elements

--- src/components/FormHeader/FormHeader.tsx
+++ src/components/FormHeader/FormHeader.tsx
@@ -3,12 +3,16 @@ import Typography from "../Typography/Typography.tsx";
 const FormHeader = () => {
     return (
         <div className='form-header d-flex flex-column align-center'>
-            <Typography className='form-title' >
-                Registration Form
-            </Typography>
-            <Typography className='form-text' variant='h3'>
-                Please fill out this form with the required information
-            </Typography>
+            <div>
+                <Typography className='form-title text-large' >
+                    Registration Form
+                </Typography>
+            </div>
+            <div>
+                <Typography className='form-text text-medium' variant='h3'>
+                    Please fill out this form with the required information
+                </Typography>
+            </div>
         </div>
     )
 }
