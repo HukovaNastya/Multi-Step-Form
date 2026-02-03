cbd8c2d34efbf7683317aa0ee62e74a8f471a077
HukovaNastya <hukovanasta@gmail.com>
2026-02-03 17:53:20 +0200

    added_localStorage_functional

---
+++ src/hooks/useStorage.tsx
@@ -0,0 +1,39 @@
+const Local_Storage_Keys ={
+    Name: 'name',
+    Email: 'email',
+    Age: 'age',
+    Interest: 'interest',
+    Description: 'description'
+}
+
+const getItem = (key:string) => {
+    if(!key || typeof key !== 'string'){
+        console.warn(`The${key} is not valid!`);
+        return;
+    }
+
+    const value = localStorage.getItem(key);
+
+    try {
+        return value ? JSON.parse(value) : null;
+    }catch {
+        return value;
+    }
+}
+
+const setItem = ({key, value}: {key: string; value:any;}) => {
+    if(!key || typeof key !== 'string'){
+        console.warn(`The ${key} is not valid!`);
+        return;
+    }
+
+    if(!value){
+        console.warn(`The ${value} is not valid`);
+        return;
+    }
+
+    const data = typeof value === 'object' ? JSON.stringify(value) : value;
+    localStorage.setItem(key, data);
+}
+
+export default {Local_Storage_Keys, getItem, setItem}
