30a7063014f104a206dc2059c2df28a99b91c16e
Denys Meshcheryakov <denis.m.pcspace@gmail.com>
2026-02-05 07:45:51 +0100

    chore: meeting notes

--- src/components/RegistrationFormContent/index.tsx
+++ src/components/RegistrationFormContent/index.tsx
@@ -2,7 +2,7 @@ import ContentForFirstStep from "./ContentForFirstStep.tsx";
 import ContentForSecondStep from "./ContentForSecondStep.tsx";
 import ContentForThirdStep from "./ContentForThirdStep.tsx";
 
-const RegistrationFormContent = ({step}:any) => {
+const RegistrationFormContent = ({step}:{step: number}) => {
     return (
         <div className='form-inner-wrapper'>
 
@@ -15,3 +15,38 @@ const RegistrationFormContent = ({step}:any) => {
 }
 
 export default RegistrationFormContent;
+
+
+// import ContentForFirstStep from "./ContentForFirstStep.tsx";
+// import ContentForSecondStep from "./ContentForSecondStep.tsx";
+// import ContentForThirdStep from "./ContentForThirdStep.tsx";
+//
+// export const Steps = {
+//     FirstStep: "FirstStep",
+//     SecondStep: "SecondStep",
+//     ThirdStep: "ThirdStep"
+// } as const;
+//
+// export type Step = keyof typeof Steps;
+//
+// const ComponentsMapping = {
+//     [Steps.FirstStep]: ContentForFirstStep,
+//     [Steps.SecondStep]: ContentForSecondStep,
+//     [Steps.ThirdStep]: ContentForThirdStep
+// }
+//
+// type RegistrationFormContentProps = {
+//     step: Step;
+// }
+//
+// const RegistrationFormContent = ({ step }: RegistrationFormContentProps) => {
+//     const Component = ComponentsMapping[step]
+//
+//     return (
+//         <div className='form-inner-wrapper'>
+//             <Component/>
+//         </div>
+//     )
+// }
+//
+// export default RegistrationFormContent;
